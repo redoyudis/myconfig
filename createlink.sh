1573143a40d5056d781147700bd9f5dd9134d069
redoyudis <redho.yudis@gmail.com>
2024-11-25 15:02:11 +0700

    change alacritty behavior

--- createlink.sh
+++ createlink.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-rm -rf ~/.bashrc ~/.config/alacritty ~/.config/nvim ~/.tmux ~/.tmux.conf
+rm -rf ~/.bashrc ~/.config/nvim ~/.tmux ~/.tmux.conf
 
 ln -s ~/myconfig/.bashrc ~/.bashrc
-ln -s ~/myconfig/alacritty ~/.config/alacritty
+ln -s ~/myconfig/alacritty.toml ~/.config/alacritty
 ln -s ~/myconfig/nvim ~/.config/nvim
 ln -s ~/myconfig/.tmux ~/.tmux
 ln -s ~/myconfig/.tmux.conf ~/.tmux.conf
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
