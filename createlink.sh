e203d780dfe9459faafdb3b405485db537e30805
redoyudis <redho.yudis@gmail.com>
2025-05-01 14:58:26 +0700

    add neofetch plus edit symlink

--- createlink.sh
+++ createlink.sh
@@ -7,5 +7,6 @@ ln -s ~/myconfig/alacritty.toml ~/.config/alacritty
 ln -s ~/myconfig/nvim ~/.config/nvim
 ln -s ~/myconfig/.tmux ~/.tmux
 ln -s ~/myconfig/.tmux.conf ~/.tmux.conf
+ln -s ~/myconfig/neofetch/config.conf ~/.config/neofetch/config.conf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
