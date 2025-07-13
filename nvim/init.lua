2ed38cfd05df861e1074bcf7f38ee5b163d83d45
redoyudis <redho.yudis@gmail.com>
2025-07-13 20:31:58 +0700

    gopls fix config to add build tag integration

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,11 @@ require("config.lazy")
 vim.o.termguicolors = true
 vim.cmd.highlight({ "Folded", "guibg=None" })
 
-require("lspconfig").gopls.setup({
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup({
   settings = {
     gopls = {
-      buildFlags = { "-tags=integration" },
+      env = { GOFLAGS = "-tags=integration" },
     },
   },
 })
